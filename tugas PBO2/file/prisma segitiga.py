bb2bc21abfc0e9a597404c6068a4e2830eb60e2e
devansky <63612294+devansky@users.noreply.github.com>
2023-10-26 21:34:56 +0700

    Add files via upload

---
+++ tugas PBO2/file/prisma segitiga.py
@@ -0,0 +1,12 @@
+print("menghitung luas dan volume prisma segitiga")
+#variable
+s=int(input("masukan sisi"))
+T=int(input("masukan Tinggi prisma"))
+
+ls =(s+s+s)*T
+lv =1/2*T
+
+
+#output
+print("hasil luas=",ls)
+print("hasil volume=",lv)
---
+++ tugas PBO2/file/volume kerucut.py
@@ -0,0 +1,9 @@
+print("mencari volume kerucut")
+
+r=int(input("masukan jari jari=" ))
+t=int(input("masukan tinggi=" ))
+
+phi = 22/7
+luas = 1/3*phi*t*(r*r)
+
+print("volume kerucut adalah=",luas)
